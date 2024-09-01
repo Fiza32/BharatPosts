4806aea1beef84dab50abd9bd042f0f127e24775
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:45:06 +0530

    Few updates in CategoryService interface

--- BloggingApp/src/main/java/com/blog/services/CategoryService.java
+++ BloggingApp/src/main/java/com/blog/services/CategoryService.java
@@ -5,21 +5,9 @@ import java.util.List;
 import com.blog.payloads.CategoryDto;
 
 public interface CategoryService {
-	// You don't have to explicitly use 'public' modifier
-	// In interface all methods are by default public
-	
-	// create
 	CategoryDto createCategory(CategoryDto categoryDto);
-	
-	// get
 	CategoryDto getCategoryById(Integer catId);
-	
-	// getAll
 	List<CategoryDto> getAllCategories();
-	
-	// update
-	CategoryDto updateCategoryById(CategoryDto categoryDto, Integer catId);
-	
-	// delete
+	CategoryDto updateCategory(CategoryDto categoryDto, Integer catId);
 	void deleteCategory(Integer catId);
 }
