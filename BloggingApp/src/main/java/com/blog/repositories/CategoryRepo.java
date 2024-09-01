cf6bbedbc9869885acc248e7e4e6310e19c38722
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:50:23 +0530

    Few changes in CategoryRepo

--- BloggingApp/src/main/java/com/blog/repositories/CategoryRepo.java
+++ BloggingApp/src/main/java/com/blog/repositories/CategoryRepo.java
@@ -1,9 +1,12 @@
 package com.blog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blog.model.Category;
 
 public interface CategoryRepo extends JpaRepository<Category, Integer> {
 
+	Optional<Category> findByTitle(String title);
 }
