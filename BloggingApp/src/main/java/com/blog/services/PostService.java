bb9eb6ec7c26d0be932787a3c781961c8be72d2b
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:44:11 +0530

    Few updates in PostService interface

--- BloggingApp/src/main/java/com/blog/services/PostService.java
+++ BloggingApp/src/main/java/com/blog/services/PostService.java
@@ -2,33 +2,18 @@ package com.blog.services;
 
 import java.util.List;
 
-import com.blog.exceptions.UserNotFoundException;
 import com.blog.payloads.PostDto;
 import com.blog.payloads.PostResponse;
 
 public interface PostService {
 
-	// Create
-	PostDto createPost(PostDto postDto, Integer userId, Integer categoryId) throws UserNotFoundException;
-	
-	// Update
-	PostDto updatePost(PostDto postDto, Integer postId) throws UserNotFoundException;
-	
-	// Get 
-	PostDto getPostById(Integer postId) throws UserNotFoundException;
-	
-	// Get All Posts
+	PostDto createPost(PostDto postDto, Integer userId, Integer categoryId);
+	PostDto updatePost(PostDto postDto, Integer postId);
+	PostDto getPostById(Integer postId);
 	PostResponse getAllPosts(int pageNum, int pageSize, String sortBy, String sortDirection);
+	void deletePost(Integer postId);
 	
-	// Get All Posts By Category
-	List<PostDto> getAllPostsByCategory(Integer categoryId) throws UserNotFoundException;
-	
-	// Get All Posts By User
-	List<PostDto> getAllPostsByUser(Integer userId) throws UserNotFoundException;
-	
-	// Search in a Post
+	List<PostDto> getAllPostsByCategory(Integer categoryId);
+	List<PostDto> getAllPostsByUser(Integer userId);
 	List<PostDto> searchPost(String keyword);
-	
-	// Delete a Post
-	void deletePost(Integer postId) throws UserNotFoundException;
 }
