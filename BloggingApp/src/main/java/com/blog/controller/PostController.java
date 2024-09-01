9d7b9bdd144c265b2714c5e91b248826ee85803b
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:41:38 +0530

    Few updates in PostController class

--- BloggingApp/src/main/java/com/blog/controller/PostController.java
+++ BloggingApp/src/main/java/com/blog/controller/PostController.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.blog.config.AppConstants;
-import com.blog.exceptions.UserNotFoundException;
 import com.blog.payloads.ApiResponse;
 import com.blog.payloads.PostDto;
 import com.blog.payloads.PostResponse;
@@ -30,71 +27,72 @@ import com.blog.services.FileService;
 import com.blog.services.PostService;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/posts")
+@RequiredArgsConstructor
 public class PostController {
 	
-	@Autowired
-	private PostService postService;
+	private final PostService postService;
+	private final FileService fileService;
+	private final String path;
 	
-	@Autowired
-	private FileService fileService;
-	
-	@Value("${project.image}")
-	private String path;
-	
-	@PostMapping("/user/{userId}/category/{categoryId}/posts")
-	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId, @PathVariable Integer categoryId) throws UserNotFoundException{
-		PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
+	@PostMapping("/user/{userId}/category/{categoryId}/")
+	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, @PathVariable Integer userId, @PathVariable Integer categoryId){
+		PostDto createPost = postService.createPost(postDto, userId, categoryId);
 		
-		return new ResponseEntity<PostDto>(createPost, HttpStatus.CREATED);
+		return new ResponseEntity<>(createPost, HttpStatus.CREATED);
 	}
 	
-	@GetMapping("/user/{userId}/posts")
-	public ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId) throws UserNotFoundException{
+	@GetMapping("/user/{userId}/")
+	public ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId) {
 		List<PostDto> posts = this.postService.getAllPostsByUser(userId);
 		
-		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
+		return new ResponseEntity<>(posts, HttpStatus.OK);
 	}
 	
-	@GetMapping("/category/{categoryId}/posts")
-	public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId) throws UserNotFoundException{
+	@GetMapping("/category/{categoryId}/")
+	public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable Integer categoryId) {
 		List<PostDto> posts = this.postService.getAllPostsByCategory(categoryId);
 		
 		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
 	}
 	
 	@GetMapping("/")
-	public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber, @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize, @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy, @RequestParam(value = "sortDirection", defaultValue = AppConstants.SORT_DIR, required = false) String sortDirection){
-		PostResponse posts = this.postService.getAllPosts(pageNumber, pageSize, sortBy, sortDirection);
+	public ResponseEntity<PostResponse> getAllPosts(
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber, 
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize, 
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy, 
+			@RequestParam(value = "sortDirection", defaultValue = AppConstants.SORT_DIR, required = false) String sortDirection
+			)
+	{
+		PostResponse posts = postService.getAllPosts(pageNumber, pageSize, sortBy, sortDirection);
 		
-		return new ResponseEntity<PostResponse>(posts, HttpStatus.OK);
+		return new ResponseEntity<>(posts, HttpStatus.OK);
 	}
 	
 	@GetMapping("/{postId}")
-	public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId) throws UserNotFoundException{
-		PostDto postDto = this.postService.getPostById(postId);
+	public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId) {
+		PostDto postDto = postService.getPostById(postId);
 		
-		return new ResponseEntity<PostDto>(postDto, HttpStatus.OK);
+		return new ResponseEntity<>(postDto, HttpStatus.OK);
 	}
 	
 	@PutMapping("/{postId}")
-	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable Integer postId) throws UserNotFoundException{
-		PostDto updatedPostDto = this.postService.updatePost(postDto, postId);
+	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable Integer postId) {
+		PostDto updatedPostDto = postService.updatePost(postDto, postId);
 		
-		return new ResponseEntity<PostDto>(updatedPostDto, HttpStatus.OK);
-		
+		return new ResponseEntity<>(updatedPostDto, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{postId}")
-	public ResponseEntity<ApiResponse> deletePost(@PathVariable Integer postId) throws UserNotFoundException {
-		this.postService.deletePost(postId);
+	public ResponseEntity<ApiResponse> deletePost(@PathVariable Integer postId) {
+		postService.deletePost(postId);
 		
-		return new ResponseEntity<ApiResponse>(new ApiResponse("Post is deleted", true), HttpStatus.OK);
+		return new ResponseEntity<>(new ApiResponse("Post is deleted", true), HttpStatus.OK);
 	}
 	
-	// Search Post by a keyword in Title
 	@GetMapping("/search/{keyword}")
 	public ResponseEntity<List<PostDto>> searchPostByTitle(@PathVariable String keyword){
 		List<PostDto> res = this.postService.searchPost(keyword);
@@ -103,22 +101,22 @@ public class PostController {
 		
 	}
 	
-	// Post Image Upload
+	// Upload an image to a Single Post
 	@PostMapping("/image/upload/{postId}")
-	public ResponseEntity<PostDto> uploadImage(@RequestParam("image") MultipartFile image, @PathVariable Integer postId) throws IOException, UserNotFoundException{
-		PostDto postDto = this.postService.getPostById(postId);
-		  
-		String fileName = this.fileService.uploadImage(path, image);
-		postDto.setImagename(fileName);
-		PostDto updatePost = this.postService.updatePost(postDto, postId);
-		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+	public ResponseEntity<PostDto> uploadImage(@RequestParam("image") MultipartFile image, @PathVariable Integer postId) throws IOException {
+		PostDto postDto = postService.getPostById(postId);
 		
+		String fileName = fileService.uploadImage(path, image);
+		postDto.setImagename(fileName);
+		
+		PostDto updatePost = postService.updatePost(postDto, postId);
+		return new ResponseEntity<>(updatePost, HttpStatus.OK);
 	}
 	
 	// Method to serve Files
 	@GetMapping(value = "/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
 	public void downloadImage(@PathVariable String imageName, HttpServletResponse response) throws IOException{
-		InputStream resource = this.fileService.getResource(path, imageName);
+		InputStream resource = fileService.getResource(path, imageName);
 		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
 		
 		StreamUtils.copy(resource, response.getOutputStream());
