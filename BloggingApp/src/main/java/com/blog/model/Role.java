af32e7364bcf8b9aa7100ca570fdfb96ea11377e
Fiza32 <mdfaizan878@gmail.com>
2024-09-01 13:35:34 +0530

    Role Enum updated

--- BloggingApp/src/main/java/com/blog/model/Role.java
+++ BloggingApp/src/main/java/com/blog/model/Role.java
@@ -1,15 +1,13 @@
 package com.blog.model;
 
 public enum Role {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int id;
-//	
-//	private String name;
-//	
-//	@ManyToMany(mappedBy = "roles")
-//	Set<User> users = new HashSet<>();
 	
 	ADMIN,
 	USER
+	
+	/*
+	public String getAuthority() {
+		return "ROLE_" + this.name();
+	}
+	*/
 }
