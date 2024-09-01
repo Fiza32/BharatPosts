55dbf4d4a8feb57bdf648726749169a85b3aa523
Fiza32 <mdfaizan878@gmail.com>
2024-09-01 16:40:05 +0530

    UserDTO updated

--- BloggingApp/src/main/java/com/blog/payloads/UserDTO.java
+++ BloggingApp/src/main/java/com/blog/payloads/UserDTO.java
@@ -1,16 +1,30 @@
 package com.blog.payloads;
 
-import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
 	
-	private int id;
+	private Integer id;
 	
+	@NotBlank(message = "Name is mandatory")
 	private String name;
+	
+	@NotBlank(message = "Email is mandatory")
+	@Email(message = "Email should be valid")
 	private String email;
+	
+	@NotBlank(message = "Password is mandatory")
+	@Size(min = 6, message = "Password should be at least 6 characters long")
 	private String password;
+	
+	@NotBlank(message = "Write few words about yourself")
 	private String about;
 }
