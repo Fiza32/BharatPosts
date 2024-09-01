5435b6c96a5b6b31a3514af512a1299f2ec87f2b
Fiza32 <mdfaizan878@gmail.com>
2024-09-01 16:39:39 +0530

    Minor changes

--- BloggingApp/src/main/java/com/blog/payloads/ApiResponse.java
+++ BloggingApp/src/main/java/com/blog/payloads/ApiResponse.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse {
 
 	private String message;
-	private boolean success;
+	private boolean action;
 }
