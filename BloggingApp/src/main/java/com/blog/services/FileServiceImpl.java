f70bb2f09399bc0fdd0d1d892f0446341c53c83b
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:43:25 +0530

    Few updates in FileServiceImpl class

--- BloggingApp/src/main/java/com/blog/services/FileServiceImpl.java
+++ BloggingApp/src/main/java/com/blog/services/FileServiceImpl.java
@@ -9,48 +9,66 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileServiceImpl implements FileService {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(FileServiceImpl.class);
 
 	@Override
 	public String uploadImage(String path, MultipartFile file) throws IOException {
-		// TODO Auto-generated method stub
 
-//		File name
-		String name = file.getOriginalFilename();
+		String originalFilename = file.getOriginalFilename();
 		
-//		Random name to generate file
-		String randomId = UUID.randomUUID().toString();
-		String fileName1 = randomId.concat(name.substring(name.lastIndexOf(".")));
-		
-//		Full Path
-		String filePath = path + File.separator + fileName1;
-		
-//		Create Folder if not created
-		File f = new File(path);
-		if(!f.exists()) {
-			f.mkdir();
+		if(originalFilename == null || originalFilename.isEmpty()) {
+			LOGGER.error("File name is invalid: {}", originalFilename);
+			throw new IOException("Invalid File name");
 		}
 		
-		// File Copy
-		Files.copy(file.getInputStream(), Paths.get(filePath));
+		String randomId = UUID.randomUUID().toString();
+		String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+		String newFilename = randomId.concat(fileExtension);
+		String filePath = path + File.separator + newFilename;
+		
+		File directory = new File(path);
+		if(!directory.exists()) {
+			LOGGER.info("Directory does not exist. Creating directory at path: {}", path);
+			Boolean dirCreated = directory.mkdir();
+			
+			if(!dirCreated) {
+				LOGGER.error("Failed to create directory at path: {}", path);
+				throw new IOException("Failed to create directory at path: " + path); 
+			}
+		}
 		
 		
-		return fileName1;
+		try {
+			Files.copy(file.getInputStream(), Paths.get(filePath));
+			LOGGER.info("File successfully uploaded: {}", newFilename);
+		} catch (IOException e) {
+			LOGGER.error("Error occured while uploading the file: {}", newFilename, e);
+			throw e;
+		}
+		
+		return newFilename;
 	}
 
 	@Override
 	public InputStream getResource(String path, String fileName) throws FileNotFoundException {
-		// TODO Auto-generated method stub
 		String fullPath = path + File.separator + fileName;
-		InputStream is = new FileInputStream(fullPath);
+		LOGGER.info("Fetching resource from path: {}", fullPath);
 		
-		// db logic to return inputStream
-		
-		return is;
+		try {
+			InputStream inputStream = new FileInputStream(fullPath);
+			LOGGER.info("Resource found: {}", fullPath);
+			return inputStream;
+		} catch (FileNotFoundException fne) {
+			LOGGER.error("Resource not found at path: {}", fullPath, fne);
+			throw fne;
+		}
 	}
-
 }
