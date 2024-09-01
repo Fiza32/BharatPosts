f9c92acc10e7a3e21d0cde25164642ef2772da24
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 01:29:42 +0530

    Few changes done in CommentDto class

--- BloggingApp/src/main/java/com/blog/payloads/CommentDto.java
+++ BloggingApp/src/main/java/com/blog/payloads/CommentDto.java
@@ -1,14 +1,18 @@
 package com.blog.payloads;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CommentDto {
 	private int id;
 	
 	@NotBlank(message = "Comment can't be empty")
+	@Size(min = 5, message = "Comment should be at least 5 characters long")
 	private String content;
 }
