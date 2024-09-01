7f7bb8fd44ae4a442c808f6eac508124cc80a2c7
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 01:24:58 +0530

    CommentServiceImpl class updated

--- BloggingApp/src/main/java/com/blog/services/CommentServiceImpl.java
+++ BloggingApp/src/main/java/com/blog/services/CommentServiceImpl.java
@@ -1,48 +1,64 @@
 package com.blog.services;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.blog.exceptions.PostNotFoundException;
+import com.blog.exceptions.ResourceNotFoundException;
 import com.blog.model.Comment;
 import com.blog.model.Post;
 import com.blog.payloads.CommentDto;
 import com.blog.repositories.CommentRepo;
 import com.blog.repositories.PostRepo;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
 
-	@Autowired
-	private PostRepo postRepo;
+	private final PostRepo postRepo;
+	private final CommentRepo commentRepo;
+	private final ModelMapper modelMapper;
 	
-	@Autowired
-	private CommentRepo commentRepo;
-	
-	@Autowired
-	private ModelMapper modelMapper;
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommentServiceImpl.class);
 	
 	@Override
-	public CommentDto createComment(CommentDto commentDto, Integer postId) throws PostNotFoundException {
-		// TODO Auto-generated method stub
+	public CommentDto createComment(CommentDto commentDto, Integer postId){
 		
-		Post post = this.postRepo.findById(postId).orElseThrow(() -> new PostNotFoundException("Post with id " + postId + " doesn't exists"));
+		Post post = postRepo.findById(postId)
+				.orElseThrow(() -> {
+					LOGGER.error("Post not found with id: {}", postId);
+					return new ResourceNotFoundException("Post not found with id: " + postId);
+				});
 		
-		Comment comment = this.modelMapper.map(commentDto, Comment.class);
+		Comment comment = convertToEnttiy(commentDto);
 		comment.setPost(post);
 		
-		Comment savedComment = this.commentRepo.save(comment);
+		Comment savedComment = commentRepo.save(comment);
 		
-		return this.modelMapper.map(savedComment, CommentDto.class);
+		LOGGER.info("Comment with id {} successfully added", savedComment.getId());
+		return convertToDto(savedComment);
 	}
 
 	@Override
-	public void deleteComment(Integer commentId) throws PostNotFoundException {
-		// TODO Auto-generated method stub
-		Comment comment = this.commentRepo.findById(commentId).orElseThrow(() -> new PostNotFoundException("Comment with id " + commentId + " doesn't exists"));
+	public void deleteComment(Integer commentId){
+		Comment comment = commentRepo.findById(commentId)
+				.orElseThrow(() -> {
+					LOGGER.error("Comment not found with id: {}", commentId);
+					return new ResourceNotFoundException("Comment not found with id: " + commentId);
+				});
 		
-		this.commentRepo.delete(comment);
+		commentRepo.delete(comment);
+		LOGGER.info("Comment with id {} successfully deleted", commentId);
 	}
-
+	
+	private CommentDto convertToDto(Comment comment) {
+		return modelMapper.map(comment, CommentDto.class);
+	}
+	
+	private Comment convertToEnttiy(CommentDto commentDto) {
+		return modelMapper.map(commentDto, Comment.class);
+	}
 }
