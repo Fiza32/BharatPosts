cc7c6497ece4550dcb29b6a8c6dfeaf1e1c8babe
Fiza32 <mdfaizan878@gmail.com>
2024-09-02 00:42:48 +0530

    Few updates in FileService class

--- BloggingApp/src/main/java/com/blog/services/FileService.java
+++ BloggingApp/src/main/java/com/blog/services/FileService.java
@@ -9,5 +9,4 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FileService {
 	String uploadImage(String path, MultipartFile file) throws IOException;
 	InputStream getResource(String path, String fileName) throws FileNotFoundException;
-	
 }
